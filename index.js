a9d2b0849ef0fd4bf59d4206924c17f60927dd49
Evgeniy <web-monu@yandex.ru>
2019-11-08 14:54:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const myPosition = API.getCurrentPosition();
-API.move(myPosition.x, myPosition.y);
+function rand() {
+  return Math.round(Math.random());
+}
+
+let { x, y } = API.getCurrentPosition();
+
+if (rand()) {
+  x = rand() ? x + 1 : x - 1;
+} else {
+  y = rand() ? y + 1 : y - 1;
+}
+
+API.move(x, y);
