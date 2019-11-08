a3b134a21900b3d2f08becdb0adb5ebb1139bf92
Evgeniy <web-monu@yandex.ru>
2019-11-08 17:50:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 let { x, y } = API.getCurrentPosition();
 let actionPoints = API.getActionPointsCount();
 let enemies = API.getEnemies();
-
 let enemiesNear = 0;
+
 enemies.forEach(enemy => {
   enemy.distanse = getDistance(x, y, enemy.position);
   if (enemy.distanse.total <= actionPoints) {
@@ -12,54 +11,43 @@ enemies.forEach(enemy => {
 });
 
 if (enemiesNear == 0) {
-  // move в сторону убегаем (к углам)
+  // бежим в угол
   let xVector = 0, yVector = 0;
-  if (x < 5 && x != 0) {
-    xVector = x * (-1);
+  if (x <= 4) {
+    xVector = 0 - x;
   } else {
-    if (x != 9) {
-      xVector = 9 - x;
-    }
+    xVector = 9 - x;
   }
-  if (y < 5 && y != 0) {
-    yVector = y * (-1);
+
+  if (y <= 4) {
+    yVector = 0 - y;
   } else {
-    if (x != 9) {
-      yVector = 9 - y;
-    }
+    yVector = 9 - y;
   }
+
   if (xVector == 0 && yVector == 0) {
-    if (x == 9) {
-      API.move(-1, 0);
+    if (x == 0) {
+      API.move(1, y);
     } else {
-      API.move(1, 0);
+      API.move(8, y);
     }
   } else {
-    let totalMoves = Math.abs(xVector) + Math.abs(yVector);
-    if (totalMoves <= actionPoints) {
-      API.move(xVector, yVector);
+    if (actionPoints <= xVector) {
+      API.move(x + Math.sign(xVector) * actionPoints, y);
     } else {
-      if (actionPoints < Math.abs(xVector)) {
-        API.move(Math.sign(xVector) * actionPoints, 0);
+      let yPoints = actionPoints - Math.abs(xVector);
+      if (yPoints <= yVector) {
+        API.move(x + xVector, y + Math.sign(yVector) * yPoints);
       } else {
-        if (actionPoints == Math.abs(xVector)) {
-          API.move(xVector, 0);
-        } else {
-          let yPoints = actionPoints - Math.abs(xVector);
-          API.move(xVector, yPoints * Math.sign(yVector));
-        }
-      };
+        API.move(x + xVector, y + yVector);
+      }
     }
   }
 
 } else {
-  // move жрем противника
-  let { xE, yE} = getNearEmenyPosition(actionPoints, enemies);
-  let xVector = xE-x;
-  let yVector = yE-y;
-
-  API.move(xVector, yVector);
-
+  // едим врага
+  let { eX, eY } = getNearEmenyPosition(actionPoints, enemies);
+  API.move(eX, eY);
 }
 
 function getDistance(x, y, enemyPoints) {
