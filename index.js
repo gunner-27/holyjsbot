0c9302848b32f9404fbd5533876c0423aa6c3d3e
Evgeniy <web-monu@yandex.ru>
2019-11-09 19:15:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,153 +1,106 @@
 let { x, y } = API.getCurrentPosition();
 let actionPoints = API.getActionPointsCount();
 let enemies = API.getEnemies();
-let enemiesNear = 0;
+let enemiesNear = [];
+let nearEnemiesCount = 0;
+let newX, newY;
 
 enemies.forEach(enemy => {
   enemy.distance = getDistance(x, y, enemy.position);
-  if (enemy.distance < actionPoints) {
-    enemiesNear++;
+  if (enemy.distance < 4) {
+    nearEnemiesCount++;
+    enemiesNear.push(enemy);
   }
 });
 
-if (enemiesNear == 0) {
-  let places = findSafePlaces(x, y);
-  if (places.length != 0) {
+if (nearEnemiesCount == 0) {
+  if (enemies.length == 1) {
+    let places = findSafePlaces(x, y);
     let place = findBestPlace(places);
-    API.move(place.x, place.y);
+    newX = place.x, newY = place.y;
+    API.move(newX, newY);
+    //hunt();
+  } else {
+    let places = findSafePlaces(x, y);
+    console.log(places);
+    let place = findBestPlace(places);
+
+    console.log(place);
+    newX = place.x, newY = place.y;
+    API.move(newX, newY);
   }
+
 } else {
-  let xNew, yNew;
   let flag = 1;
-  enemies.forEach(enemy => {
-    enemy.distance = getDistance(x, y, enemy.position);
-    if (enemy.distance < actionPoints) {
-      if (isSafePlace(enemy.position.x, enemy.position.y)) {
-        flag = 0;
-        xNew = enemy.position.x;
-        yNew = enemy.position.y;
-      }
+  enemiesNear.forEach(enemy => {
+    if (isSafePlace(enemy.position.x, enemy.position.y)) {
+      newX = enemy.position.x;
+      newY = enemy.position.y;
+      flag = 0;
     }
   });
   if (flag) {
     let places = findSafePlaces(x, y);
     if (places.length != 0) {
       let place = findBestPlace(places);
-      xNew = place.x;
-      yNew = place.y;
+      newX = place.x, newY = place.y;
     } else {
-      let { mX, mY } = getNearEnemy(enemies, actionPoints);
-      xNew = mX;
-      yNew = mY;
+      newX = enemiesNear[0].position.x;
+      newY = enemiesNear[0].position.y;
     }
   }
-  API.move(xNew, yNew);
+  API.move(newX, newY);
 }
 
 function getDistance(x, y, enemyPosition) {
-  let xDistance, yDistance, total;
-  xDistance = enemyPosition.x > x ? enemyPosition.x - x : x - enemyPosition.x;
-  yDistance = enemyPosition.y > y ? enemyPosition.y - y : y - enemyPosition.y;
-  total = xDistance + yDistance;
+  let total;
+  total = Math.abs(enemyPosition.x - x) + Math.abs(enemyPosition.y - y);
   return total;
 }
 
-function getNearEnemy(enemies, actionPoints) {
-  let nearEnemies = [];
-  let maxEnemies = 10;
-  let mX, mY;
-  enemies.forEach(enemy => {
-    if (enemy.distance < actionPoints) {
-      nearEnemies.push(enemy);
-    }
-  });
-  nearEnemies.forEach(enemy => {
-    enemy.enemiesAround = countNearEnemies(enemy.position.x, enemy.position.y, actionPoints);
-    if (enemy.enemiesAround < maxEnemies) {
-      maxEnemies = enemy.enemiesAround;
-      mX = enemy.position.x;
-      mY = enemy.position.y;
-    }
-  });
-  return ({ mX, mY });
-}
-
-function countNearEnemies(x, y, actionPoints) {
-  let enemiesNear = 0;
+function isSafePlace(x, y) {
+  let nearEnemiesC = 0;
   enemies.forEach(enemy => {
     enemy.distance = getDistance(x, y, enemy.position);
-    if (enemy.distance < actionPoints && enemy.distance > 0) {
-      enemiesNear++;
+    if (enemy.distance < 4) {
+      if (enemy.position.x != x && enemy.position.y != y) {
+        nearEnemiesC++;
+      }
     }
   });
-  return enemiesNear;
-}
-
-function findNearCorner(x, y) {
-  let xm, ym;
-  if (x < 4) {
-    xm = 0;
+  if (nearEnemiesC == 0) {
+    return true;
   } else {
-    xm = 8;
-  };
-  if (y < 4) {
-    ym = 0;
-  } else {
-    ym = 8;
+    return false;
   }
-  return ({ xm, ym });
 }
-
 
 function findSafePlaces(x, y) {
   let places = findAvailablePlaces(x, y);
   let safePlaces = [];
   places.forEach(place => {
-    let enemiesAround = countNearEnemies(place.x, place.y, 4);
-    if (enemiesAround == 0) {
+    if (isSafePlace(place.x, place.y)) {
       safePlaces.push(place);
     }
   });
+
   return safePlaces;
 }
 
 function findAvailablePlaces(x, y) {
   let places = [];
-  let xMin, xMax, yMin, yMax;
-  if (x > 2) {
-    xMin = x - 3;
-  } else {
-    xMin = 0;
-  }
-  if (x < 6) {
-    xMax = x + 3;
-  } else {
-    xMax = 8;
-  }
+  let xMin = x > 2 ? x - 3 : 0;
+  let xMax = x < 6 ? x + 3 : 8;
+  let yMin = y > 2 ? y - 3 : 0;
+  let yMax = y < 6 ? y + 3 : 8;
 
-  if (y > 2) {
-    yMin = y - 3;
-  } else {
-    yMin = 0;
-  }
-  if (y < 6) {
-    yMax = y + 3;
-  } else {
-    yMax = 8;
-  }
-
-  for (let i = xMin; i < xMax; i++) {
-    for (let j = yMin; j < yMax; j++) {
-      places.push({ x: i, y: j });
+  for (let i = xMin; i < xMax + 1; i++) {
+    for (let j = yMin; j < yMax + 1; j++) {
+      if (i != x && j != y) {
+        places.push({ x: i, y: j });
+      }
     }
   }
-  let pos = 0;
-  places.forEach((element, i) => {
-    if (element.x == x && element.y == y) {
-      pos = i;
-    }
-  });
-  places.splice(pos, 1);
   return (places);
 }
 
@@ -165,12 +118,12 @@ function findBestPlace(places) {
   return bestplace;
 }
 
-function isSafePlace(x, y) {
-  let nearEnemies = countNearEnemies(x, y, 3);
-  if (nearEnemies == 0) {
-    return true;
-  } else {
-    return false;
-  }
+function findNearCorner(x, y) {
+  let xm = x < 4 ? 0 : 8;
+  let ym = y < 4 ? 0 : 8;
+  return ({ xm, ym });
 }
-© 2019 GitHub, Inc.
+
+// function hunt() {
+
+// }
